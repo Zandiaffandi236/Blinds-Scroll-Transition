8134c3fb79467c398f2c6897b6465e96823f3c80
SlabPixel Designer <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-15 11:50:43 +0700

    initial commit

---
+++ src/slides.js
@@ -0,0 +1,33 @@
+const slides = [
+    {
+      title: "Desert Oasis Pool",
+      image: "/src/assets/img-1.jpg",
+      url: "#",
+    },
+
+    {
+      title: "Domed Sactuary",
+      image: "/src/assets/img-2.jpg",
+      url: "#",
+    },
+  
+    {
+      title: "Courtyard Retreat",
+      image: "/src/assets/img-3.jpg",
+      url: "#",
+    },
+  
+    {
+      title: "Arched Corridor",
+      image: "/src/assets/img-4.jpg",
+      url: "#",
+    },
+  
+    {
+      title: "Illuminated Grotto",
+      image: "/src/assets/img-5.jpg",
+      url: "#",
+    },
+  ];
+  
+  export default slides;
